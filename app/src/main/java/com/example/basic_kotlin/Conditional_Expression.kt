cc8239fdf067edd1f874cd123afd48ebd89bcabf
MuhSaktiR <seken876@gmail.com>
2024-10-13 14:35:33 +0700

    Membuat fungsi ConditionalStatement dan kondisional if-else jika > 0 maka positif dan jika selain itu hasilnya negatif

--- app/src/main/java/com/example/basic_kotlin/Conditional_Expression.kt
+++ app/src/main/java/com/example/basic_kotlin/Conditional_Expression.kt
@@ -4,3 +4,15 @@ package com.example.basic_kotlin
 // Conditional expression adalah ekpresi yang mengembalikan nilai berdasarkan kondisi tertentu
 // Conditional expression menggunakan if-else, when, dan try-catch
 
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    // If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    }
+    else {
+        println("Negative Number")
+    }
+}
