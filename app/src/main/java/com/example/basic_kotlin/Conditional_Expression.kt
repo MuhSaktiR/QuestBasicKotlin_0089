df13e8d13c5fc6e877ca4f80f26c8d9a70c669a9
MuhSaktiR <seken876@gmail.com>
2024-10-13 14:22:21 +0700

    Membuat File Conditional Expression

---
+++ app/src/main/java/com/example/basic_kotlin/Conditional_Expression.kt
@@ -0,0 +1,6 @@
+package com.example.basic_kotlin
+
+// Conditional Expression
+// Conditional expression adalah ekpresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
