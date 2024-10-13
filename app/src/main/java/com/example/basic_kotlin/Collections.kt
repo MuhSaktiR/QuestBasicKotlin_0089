5213f51e3e394766ce5a6db143621ef32712b58c
MuhSaktiR <seken876@gmail.com>
2024-10-13 13:54:07 +0700

    Membuat variable map Mutable yang berarti bisa diubah dengan key string dan nilai int

--- app/src/main/java/com/example/basic_kotlin/Collections.kt
+++ app/src/main/java/com/example/basic_kotlin/Collections.kt
@@ -84,6 +84,22 @@ fun ContohMap(){
     // Map Read-Only
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
 }
 
 fun main(){
