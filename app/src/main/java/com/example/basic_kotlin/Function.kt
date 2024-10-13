27fdbf09c3a9d8d71a723545be6258506909b600
MuhSaktiR <seken876@gmail.com>
2024-10-13 16:11:20 +0700

    Memperbarui fun withReturn terjadi kesalahan penulisan

--- app/src/main/java/com/example/basic_kotlin/Function.kt
+++ app/src/main/java/com/example/basic_kotlin/Function.kt
@@ -27,8 +27,8 @@ fun withDefaultParameter(name: String = "Indra", age: Int) {
     println("Hello, $name! You are $age years old.")
 }
 
-fun withReturn(panjang: Int, lebar: Int): Int {
-    return panjang * lebar
+fun withReturn(panjang : Int, lebar : Int) {
+    return println(panjang * lebar)
 }
 
 fun main() {
