98d551a86617e6fb70e12cb76a798809183e946b
MuhSaktiR <seken876@gmail.com>
2024-10-13 12:28:50 +0700

    Melanjutkan Activity Basic Kotlin dari Kelas

---
+++ app/src/main/java/com/example/basic_kotlin/Collections.kt
@@ -0,0 +1,39 @@
+package com.example.basic_kotlin
+
+fun ContohList() {
+    println("=== List ===")
+    //List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data List Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+    println(shape.first())
+    println(shape.last())
+    println(shape.count())
+
+    shape.removeAt(1)
+    println(shape)
+
+    // Mengubah data di dalam List Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    // List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
+
+fun main(){
+    println(ContohList())
+}
